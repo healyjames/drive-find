1996229c7d37169e7dffc739309232e331a5246c
James <healyjames97@gmail.com>
2024-08-08 21:16:11 +0100

    feat: add brand colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -7,14 +7,21 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: {
+        light: '#F5933D',
+        base: '#E8740C',
+        dark: '#AE5609',
       },
+      secondary: {
+        light: '#264569',
+        base: '#132335',
+        dark: '#050A0F'
+      }
     },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
