91927af778c0f63ba838cc427ff79014d1717e61
James <healyjames97@gmail.com>
2024-08-09 21:25:40 +0100

    chore: remove previous loading page test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import { LogoAnimation } from "./components/logo-animation";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LogoAnimation />
     </main>
   );
 }
