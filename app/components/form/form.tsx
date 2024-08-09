5d0c7ff097e1d00344206b1d85953a95745d4524
James <healyjames97@gmail.com>
2024-08-09 22:28:42 +0100

    chore: initial changes for form elements

---
+++ app/components/form/form.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+interface FormProps {
+    className: string
+}
+
+export const Form = (props: FormProps) => {
+
+    return (
+        <React.Fragment>
+            <form className={`${props.className} w-full px-10`}>
+                <div className="mb-2">
+                    <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your location</label>
+                    <input type="text" id="location" className="form-input-light" placeholder="Somewhere..." required />
+                </div>
+                <button type="submit" className="btn-light">Go</button>
+            </form>
+        </React.Fragment>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+import Image from "next/image"
+
+import { Form } from "./components/form/form";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-screen min-h-screen w-full bg-primary-base p-4">
+      <div className="h-full grid grid-col grid-rows-3 justify-items-center">
+        <Image
+          src="/logo/primary.svg"
+          alt=""
+          width={120}
+          height={120}
+          className="self-start"
+        />
+        <Form
+          className="self-center"
+        />
+        <div className="self-end">
+          <p>socials</p>
+        </div>
+      </div>
     </main>
   );
 }
