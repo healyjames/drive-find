6a8438282c7d3017848ae3a42c349d4c37aadd7a
James <healyjames97@gmail.com>
2024-08-09 22:15:53 +0100

    fix: tailwind config for bg colours failing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,19 +7,20 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      primary: {
-        light: '#F5933D',
-        base: '#E8740C',
-        dark: '#AE5609',
+    extend:{
+      colors: {
+        primary: {
+          light: '#F5933D',
+          base: '#E8740C',
+          dark: '#AE5609',
+        },
+        secondary: {
+          light: '#5D6267',
+          base: '#1A1D1F',
+          dark: '#111315'
+        }
       },
-      secondary: {
-        light: '#5D6267',
-        base: '#1A1D1F',
-        dark: '#111315'
-      }
     },
-    extend: {},
   },
   plugins: [],
 }
