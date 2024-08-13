687dcc316c1c566a815942d11d1137be4a694b79
James <healyjames97@gmail.com>
2024-08-13 18:02:00 +0100

    chore: improve camera icon usability

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
     <main className="h-screen min-h-screen w-full bg-primary-base lg:grid lg:grid-col grid-cols-home">
       <div className="relative hidden lg:block">
         <div className="relative z-10 m-2 px-4 py-2 flex flex-row gap-2 items-center opacity-40">
-          <CameraIcon />
+          <CameraIcon
+            viewBox="0 0 512 512"
+            width="16"
+            height="16"
+          />
           <p className="text-xs">Photo by <a href="https://unsplash.com/@grebenyev?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vlad Grebenyev</a></p>
         </div>
         
