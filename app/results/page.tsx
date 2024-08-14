6106b18e4d9a2a832d3898f546def33bdc2e28a5
James <healyjames97@gmail.com>
2024-08-14 16:22:02 +0100

    chore: wip: add header element to map

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,8 +1,10 @@
 import { Map } from '../components/map/map'
+import Header from '../components/header/page'
 
 export default function Results() {
     return (
         <div>
+            <Header />
             <Map />
         </div>
     )
