63f7b3fd0e778da67e8d845e4f78ecf73e96d393
James <healyjames97@gmail.com>
2024-08-14 16:41:47 +0100

    feat: initial styles for header overlay

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,3 +1,59 @@
+import Image from "next/image"
+
+import { HamburgerIcon } from "../icons/icon"
+
 export default function Header() {
-    return <h1>Hello, Header!</h1>
+    return (
+        <div className="absolute top-0 left-0 w-full z-10 flex justify-between items-center p-4 bg-secondary-base shadow">
+            <div className="navbar-start">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                    <HamburgerIcon />
+                </div>
+            </div>
+            <div>
+                <a href="/" className="btn btn-ghost text-xl">
+                    <Image
+                        src="/logo/primary.svg"
+                        alt=""
+                        width={75}
+                        height={120}
+                        className="self-start"
+                    />
+                </a>
+            </div>
+            <div className="flex flex-row flex-nowrap gap-1">
+                <button className="px-1 py-2 cursor-not-allowed">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+                <button className="px-1 py-2 cursor-not-allowed">
+                    <div className="indicator">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                        </svg>
+                        <span className="badge badge-xs badge-primary indicator-item"></span>
+                    </div>
+                </button>
+            </div>
+        </div>
+    )
 }
--- app/components/icons/icon.tsx
+++ app/components/icons/icon.tsx
@@ -53,10 +53,19 @@ const XIcon = () => (
     </IconContainer>
 )
 
-
+const HamburgerIcon = () => (
+    <IconContainer viewBox="0 0 24 24" fill="none" stroke="currentColor">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h7" />
+    </IconContainer>
+)
 
 export {
     FacebookIcon,
     InstagramIcon,
-    XIcon
+    XIcon,
+    HamburgerIcon
 }
