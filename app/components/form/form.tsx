9cce7c368f0e5f0f17eac7398fa073390880ea7f
James <healyjames97@gmail.com>
2024-08-14 16:09:31 +0100

    chore: post form data to results page

--- app/components/form/form.tsx
+++ app/components/form/form.tsx
@@ -8,7 +8,7 @@ export const Form = (props: FormProps) => {
 
     return (
         <React.Fragment>
-            <form className={`${props.className} max-w-md w-full px-4`}>
+            <form action="/results" method="POST" className={`${props.className} max-w-md w-full px-4`}>
                 <div className="mb-2">
                     <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your location</label>
                     <input type="text" id="location" className="form-input-light" placeholder="Somewhere..." required />
