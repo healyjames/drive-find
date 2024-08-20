0dcab4084e9d98685902c071ce9c891a22f3f9c7
James <healyjames97@gmail.com>
2024-08-20 20:40:52 +0100

    chore: wrap form in provider

--- app/components/form/form.tsx
+++ app/components/form/form.tsx
@@ -49,13 +49,15 @@ export const Form = (props: FormProps) => {
 
     return (
         <React.Fragment>
-            <form action="/results" method="POST" className={`${props.className} max-w-md w-full px-4`}>
-                <div className="mb-2">
-                    <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your location</label>
-                    <input type="text" id="location" className="form-input-light" placeholder="Somewhere..." required />
-                </div>
-                <button type="submit" className="btn-light">Go</button>
-            </form>
+            <AutocompleteProvider>
+                <form action="/results" method="POST" className={`${props.className} max-w-md w-full px-4`}>
+                    <div className="mb-2">
+                        <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your location</label>
+                        <input type="text" id="location" className="form-input-light" placeholder="Somewhere..." required />
+                    </div>
+                    <button type="submit" className="btn-light">Go</button>
+                </form>
+            </AutocompleteProvider>
         </React.Fragment>
     )
 }
