7836c60ec7fa65662eefdfcbe79366bb458e43ec
James <healyjames97@gmail.com>
2024-08-20 21:27:11 +0100

    feat: hook up form data and map location

--- app/components/map/map.tsx
+++ app/components/map/map.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import { GoogleMap } from '@react-google-maps/api'
 
@@ -42,12 +42,24 @@ const loading = <div className="w-screen h-screen flex justify-center items-cent
 
 export const Map = () => {
 
+    const [lat, setLat] = useState<number>(mapDefaults.coordinates.lat)
+    const [lng, setLng] = useState<number>(mapDefaults.coordinates.lng)
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search)
+        setLat(Number(queryParams.get('lat')))
+        setLng(Number(queryParams.get('lng')))
+    }, [])
+
     return (
         <React.Fragment>
             <MapProvider error={error} loading={loading}>
                 <GoogleMap
                     mapContainerStyle={mapDefaults.style}
-                    center={mapDefaults.coordinates}
+                    center={{
+                        lat: lat,
+                        lng: lng
+                    }}
                     zoom={mapDefaults.zoom}
                     options={mapDefaults.options}
                 />
