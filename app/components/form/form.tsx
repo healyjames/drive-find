64f7ca12c74bec684e892437e3f684757d56a2c3
James <healyjames97@gmail.com>
2024-08-20 20:42:22 +0100

    chore: wrap input in autocomplete component

--- app/components/form/form.tsx
+++ app/components/form/form.tsx
@@ -53,7 +53,16 @@ export const Form = (props: FormProps) => {
                 <form action="/results" method="POST" className={`${props.className} max-w-md w-full px-4`}>
                     <div className="mb-2">
                         <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your location</label>
-                        <input type="text" id="location" className="form-input-light" placeholder="Somewhere..." required />
+                        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                            <input 
+                                type="text"
+                                id="location"
+                                className="form-input-light"
+                                placeholder="Somewhere..."
+                                value={input}
+                                required
+                            />
+                        </Autocomplete>
                     </div>
                     <button type="submit" className="btn-light">Go</button>
                 </form>
