5d632e89330857679711915923facfa77c39e56a
James <healyjames97@gmail.com>
2024-09-08 16:38:43 +0100

    feat: define env variable types

---
+++ environment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NEXT_PUBLIC_GOOGLE_MAP_API: string
+            ENV: string
+            GOOGLE_CLIENT_ID: string,
+            GOOGLE_CLIENT_SECRET: string,
+        }
+    } 
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
