3660b4f840f53cdc9d4ecceca3b0539c2ce05363
James <healyjames97@gmail.com>
2024-09-10 19:43:16 +0100

    chore: update with github provider

--- utils/authHelper.ts
+++ utils/authHelper.ts
@@ -2,22 +2,30 @@ import type {
     GetServerSidePropsContext,
     NextApiRequest,
     NextApiResponse,
-  } from "next"
-  import type { NextAuthOptions } from "next-auth"
-  import { getServerSession } from "next-auth"
-  
-  // You'll need to import and pass this
-  // to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
-  export const config = {
-    providers: [], // rest of your config
-  } satisfies NextAuthOptions
-  
-  // Use it in server contexts
-  export function auth(
+} from "next"
+import type { NextAuthOptions } from "next-auth"
+import { getServerSession } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+
+// You'll need to import and pass this
+// to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
+export const authConfig = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        // ...add more providers here
+    ],
+} satisfies NextAuthOptions
+
+// Use it in server contexts
+export function auth(
     ...args:
-      | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
-      | [NextApiRequest, NextApiResponse]
-      | []
-  ) {
-    return getServerSession(...args, config)
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+    return getServerSession(...args, authConfig)
 }
