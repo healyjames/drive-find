d9137f0d9020268b7d665d16c8ad8f4cf6848b90
James <healyjames97@gmail.com>
2024-09-14 23:23:24 +0100

    feat: add avater to header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['avatars.githubusercontent.com', 'ui-avatars.com'],
+    },
+};
 
 export default nextConfig;
