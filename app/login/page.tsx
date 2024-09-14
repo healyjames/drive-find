92a73b1d5c8506ac2188fa9340cfdd017ca2552b
James <healyjames97@gmail.com>
2024-09-14 22:41:17 +0100

    refactor: signout view

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,45 +18,44 @@ export default async function Login() {
         <div className='h-screen flex flex-col'>
             <Header />
             <div className='m-auto p-4 md:max-w-[676px] md:mx-auto'>
-                <div className='mb-4'>
-                    <p>Login with:</p>
-                </div>
-                <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+                {!session?.user ? (
+                    <>
+                        <div className='mb-4'>
+                            <p>Login with:</p>
+                        </div>
+                        <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
 
-                    {!session?.user ? (
-                        Object.values(providerMap).map((provider) => (
-                            <form
-                                className="[&>div]:last-of-type:hidden"
-                                key={provider.id}
-                                action={async () => {
-                                    "use server"
-                                    await signIn(provider.id, { redirectTo: "/" })
-                                }}
-                            >
-                                <ProviderSignInButton name={provider.name} />
-                            </form>
-                        ))
-                    ) : (
-                        <>
-                        <p>You are logged in</p>
-                            <form
-                                action={async () => {
-                                    "use server"
-                                    await signOut()
-                                }}
-                                className="w-full"
-                                >
-                                <button>
-                                    Sign Out
-                                </button>
-                            </form>
-                        </>
-                    )}
-                    
-                    <div className='p-4 w-full border flex justify-center bg-grey-light text-primary-base'>
-                        <p>More providers coming soon</p>
-                    </div>
-                </div>
+                            {Object.values(providerMap).map((provider) => (
+                                    <form
+                                        className="[&>div]:last-of-type:hidden"
+                                        key={provider.id}
+                                        action={async () => {
+                                            "use server"
+                                            await signIn(provider.id, { redirectTo: "/" })
+                                        }}
+                                    >
+                                        <ProviderSignInButton name={provider.name} />
+                                    </form>
+                                ))}
+                            
+                            <div className='p-4 w-full border flex justify-center bg-grey-light text-primary-base'>
+                                <p>More providers coming soon</p>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <form
+                        action={async () => {
+                            "use server"
+                            await signOut()
+                        }}
+                        className="w-full"
+                    >
+                        <button className={`p-4 w-full border flex flex-row flex-nowrap justify-center gap-4 hover:bg-primary-light hover:border-primary-light`}>
+                            <p>Sign Out</p>
+                        </button>
+                    </form>
+                )}
 
                 {/* <div className='mt-16 text-center'>
                     <p>Not got an account? <Link href='/signup' className='border-b'>Sign up here.</Link></p>
