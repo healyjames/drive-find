0eaf33cd09e14b50504fcd0138c0907f2d60cd8a
James <healyjames97@gmail.com>
2024-09-15 16:52:04 +0100

    chore: initual dropdown

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,8 +1,19 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import { HamburgerIcon } from "../icons/icon"
+import { HamburgerIcon } from "@/components/icons/icon"
 import { auth } from "@/auth"
+
+import { 
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuSeparator,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
 
 interface HeaderProps {
     outerClass?: string
@@ -12,7 +23,7 @@ export const Header = async (props: HeaderProps) => {
     const session = await auth()
 
     return (
-        <div className={`${props.outerClass} top-0 left-0 w-full z-10 flex justify-between items-center p-4 bg-primary-base shadow`}>
+        <div className={`${props.outerClass || ''} top-0 left-0 w-full z-10 flex justify-between items-center p-4 bg-primary-base shadow`}>
             <div className="navbar-start">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                     <HamburgerIcon />
@@ -31,7 +42,7 @@ export const Header = async (props: HeaderProps) => {
             </div>
             <div className="flex flex-row flex-nowrap gap-1">
                 {
-                    session?.user && (
+                    session?.user ? (
                         <Link href="/login" className='rounded-full outline-none hover:outline-secondary-base focus:outline-secondary-base'>
                             <Image
                                 src={session?.user?.image ?? `https://ui-avatars.com/api/?name=${session?.user?.name}&format=png`}
@@ -41,6 +52,21 @@ export const Header = async (props: HeaderProps) => {
                                 className="rounded-full"
                             />
                         </Link>
+                    ) : (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button>Open</Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56">
+                                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                                    <DropdownMenuItem>Settings</DropdownMenuItem>
+                                    <DropdownMenuItem>Sign out</DropdownMenuItem>
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     )
                 }
             </div>
