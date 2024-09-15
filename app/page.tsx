32bfc5198be1a00db82fbff37505c563981c3885
James <healyjames97@gmail.com>
2024-09-15 17:25:07 +0100

    feat: add nav to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Link from "next/link"
 import { Form } from "@/components/form/form"
 import { Footer } from "@/components/footer/footer"
 import { Banner } from "@/components/banner/banner"
+import { Header } from "@/components/header/header"
 
 export default function Home() {
   return (
-    <main className="h-screen min-h-screen w-full bg-primary-base">
+    <main className="h-[80vh] min-h-[80vh] w-full bg-primary-base">
       <Banner>This app is currently in Beta mode</Banner>
+      <Header />
       <div className="h-full grid grid-col grid-rows-3 justify-items-center p-4 pt-14">
         <Link href="/" className="self-center">
           <Image
