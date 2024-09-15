84e503ba1147170cffe17552b712fb067427f2d2
James <healyjames97@gmail.com>
2024-09-15 07:44:59 +0100

    fix: deprecated domains warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'avatars.githubusercontent.com', 
-            'ui-avatars.com',
-            'lh3.googleusercontent.com'
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname:'**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'ui-avatars.com',
+                port: '',
+                pathname:'**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname:'**'
+            }
         ],
     },
 };
