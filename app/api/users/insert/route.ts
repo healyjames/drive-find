f5bf9c8b5e5ceb7f6f7276cf30e13ea11824f9a9
James <healyjames97@gmail.com>
2025-01-01 22:47:31 +0000

    feat: initial changes to successfully add new user to db

--- app/api/users/insert/route.ts
+++ app/api/users/insert/route.ts
@@ -1,4 +1,26 @@
+import { Document } from "mongodb"
+import client from "@/lib/mongodb"
+
 export async function POST(request: Request) {
-    const res = await request.json()
-    return Response.json(res)
+  const res: Document = await request.json()
+
+  try {
+		await client.db("drive_find")
+			.collection("users")
+			.insertOne(res)
+
+			return new Response(JSON.stringify({
+				message: "New User Added Successfully.",
+				detials: res
+			}), {
+				status: 200,
+				headers: { "Content-Type": "application/json" },
+			})
+	} catch (e) {
+		console.error(e)
+		return new Response(JSON.stringify({ error: "Error adding new user.", details: e }), {
+			status: 500,
+			headers: { "Content-Type": "application/json" },
+		})
+	}
 }
