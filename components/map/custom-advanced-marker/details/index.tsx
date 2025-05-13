934b1f5e15cd646c2d9b40df0cb214fa8d4bef99
James <healyjames97@gmail.com>
2025-05-13 16:52:50 +0100

    refactor: lint and update css import path

--- components/map/custom-advanced-marker/details/index.tsx
+++ components/map/custom-advanced-marker/details/index.tsx
@@ -1,19 +1,19 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent } from 'react'
 
-import "./real-estate-listing-details.css";
+import './details.css'
 
 interface Props {
-    title: string;
-    description: string;
+  title: string
+  description: string
 }
 
 export const Details: FunctionComponent<Props> = ({ title, description }) => {
-    return (
-        <div className="details-container">
-            <div className="listing-content">
-                <h2>{title}</h2>
-                <p>{description}</p>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="details-container">
+      <div className="listing-content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
