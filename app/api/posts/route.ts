4f948acda0d94a34c888fb61c0ff5a381c6e01e6
James <healyjames97@gmail.com>
2025-05-27 15:56:00 +0100

    fix: type errors and linting

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,34 +1,37 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { auth } from "@/auth"
-import dbConnect from "@/lib/mongodb"
+import { NextRequest, NextResponse } from 'next/server'
+import { auth } from '@/auth'
+import dbConnect from '@/lib/mongodb'
 import Post from '@/models/Post'
-import { LOGGER } from "@/lib/utils"
+import { LOGGER } from '@/lib/utils'
 
 const LOG = LOGGER()
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-	await dbConnect()
+export async function GET(req: NextRequest, res: NextResponse) {
+  await dbConnect()
 
-	try {
-		const posts = await Post.find({})
+  try {
+    const posts = await Post.find({})
 
-			return new Response(JSON.stringify(posts), {
-				status: 200,
-				headers: { "Content-Type": "application/json" },
-			})
-	} catch (e) {
-		const err = e as Error
-		LOG.error({
-			tags: '[POSTS] [GET]',
-			message: 'Failed to get posts.',
-			path: '/api/posts',
-			error: err,
-			body: JSON.stringify(req.body)
-		})
+    return new Response(JSON.stringify(posts), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (e) {
+    const err = e as Error
+    LOG.error({
+      tags: '[POSTS] [GET]',
+      message: 'Failed to get posts.',
+      path: '/api/posts',
+      error: err,
+      body: JSON.stringify(req.body),
+    })
 
-		return new Response(JSON.stringify({ error: "Error fetching data", details: e }), {
-			status: 500,
-			headers: { "Content-Type": "application/json" },
-		})
-	}
+    return new Response(
+      JSON.stringify({ error: 'Error fetching data', details: e }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    )
+  }
 }
