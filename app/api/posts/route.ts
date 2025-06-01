bd41b1e3c5b9cba771b35fd61fc5f3084d2a0d7a
James <healyjames97@gmail.com>
2025-06-01 14:11:06 +0100

    feat: utility function to transform data to geo data

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { auth } from '@/auth'
 import dbConnect from '@/lib/mongodb'
-import Post from '@/models/Post'
+import Post, { IPost } from '@/models/Post'
 import { LOGGER } from '@/lib/utils'
+import { Feature, FeatureCollection, Point } from 'geojson'
 
 const LOG = LOGGER()
 
@@ -11,8 +12,9 @@ export async function GET(req: NextRequest, res: NextResponse) {
 
   try {
     const posts = await Post.find({})
+    const geoJson = transformIPostsToGeoJSON(posts)
 
-    return new Response(JSON.stringify(posts), {
+    return new Response(JSON.stringify(geoJson), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     })
@@ -33,5 +35,35 @@ export async function GET(req: NextRequest, res: NextResponse) {
         headers: { 'Content-Type': 'application/json' },
       },
     )
+  }
+}
+
+const transformIPostsToGeoJSON = (
+  posts: IPost[],
+): FeatureCollection<Point, IPost> => {
+  const features: Feature<Point, IPost>[] = posts
+    .map((post) => {
+      if (
+        !post.location ||
+        typeof post.location.longitude !== 'number' ||
+        typeof post.location.latitude !== 'number'
+      ) {
+        console.warn('Post with invalid location:', post.id)
+        return null
+      }
+      return {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [post.location.longitude, post.location.latitude],
+        },
+        properties: post
+      }
+    })
+    .filter((feature) => feature !== null) as Feature<Point, IPost>[]
+
+  return {
+    type: 'FeatureCollection',
+    features,
   }
 }
