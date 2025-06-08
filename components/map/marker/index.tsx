f374e648b7068a19111edb12488ccbf9f552f463
James <healyjames97@gmail.com>
2025-06-08 11:55:48 +0100

    feat: temporary marker icons

--- components/map/marker/index.tsx
+++ components/map/marker/index.tsx
@@ -31,9 +31,9 @@ export const FeatureMarker = ({
       position={position}
       onClick={handleClick}
       anchorPoint={AdvancedMarkerAnchorPoint.CENTER}
-      className={'marker feature'}
     >
-      <p>Hello</p>
+        <div className="z-10 relative w-8 h-8 bg-secondary-base rounded-full border-[3px] border-white shadow-md transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"></div>
+      <div className="h-4 w-full bg-[#000] rounded-[50%] opacity-20 -mt-1"></div>
     </AdvancedMarker>
   )
 }
