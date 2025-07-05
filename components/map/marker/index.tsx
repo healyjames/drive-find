1c9eb1bb548d62641778916fbf00eb67133d8b26
James <healyjames97@gmail.com>
2025-07-05 20:19:59 +0100

    feat: move to provider for fetching posts

--- components/map/marker/index.tsx
+++ components/map/marker/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   AdvancedMarker,
   AdvancedMarkerAnchorPoint,
@@ -6,57 +6,25 @@ import {
 } from '@vis.gl/react-google-maps'
 import { Feature, Point } from 'geojson'
 import { IPost } from '@/models/Post'
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetDescription,
-  SheetTrigger,
-} from '@/components/ui/sheet'
-import { MarkerDetails } from '@/components/marker-details'
 
 type TreeMarkerProps = {
   position: google.maps.LatLngLiteral
   feature: Feature<Point, IPost>
 }
 
-export const FeatureMarker = ({ position, feature }: TreeMarkerProps) => {
-  const [markerRef, marker] = useAdvancedMarkerRef()
-  const [isSheetOpen, setIsSheetOpen] = useState(false)
-
-  const handleClick = () => {
-    setIsSheetOpen(true)
-  }
+export const FeatureMarker = ({ position }: TreeMarkerProps) => {
+  const [markerRef] = useAdvancedMarkerRef()
 
   return (
     <>
-      <Sheet modal={false} open={isSheetOpen} onOpenChange={setIsSheetOpen}>
-        <SheetTrigger asChild>
-          <AdvancedMarker
-            ref={markerRef}
-            position={position}
-            onClick={handleClick}
-            anchorPoint={AdvancedMarkerAnchorPoint.CENTER}
-          >
-            <div className="z-10 relative w-8 h-8 bg-secondary-base rounded-full border-[3px] border-white shadow-md transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"></div>
-            <div className="h-4 w-full bg-[#000] rounded-[50%] opacity-20 -mt-1"></div>
-          </AdvancedMarker>
-        </SheetTrigger>
-        <SheetContent
-          onInteractOutside={(event) => {
-            event.preventDefault()
-          }}
-        >
-          <SheetHeader>
-            <SheetTitle>Feature Details</SheetTitle>
-            <SheetDescription>
-              Details about the selected feature.
-            </SheetDescription>
-          </SheetHeader>
-          <MarkerDetails feature={feature.properties} />
-        </SheetContent>
-      </Sheet>
+      <AdvancedMarker
+        ref={markerRef}
+        position={position}
+        anchorPoint={AdvancedMarkerAnchorPoint.CENTER}
+      >
+        <div className="z-10 relative w-8 h-8 bg-secondary-base rounded-full border-[3px] border-white shadow-md transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"></div>
+        <div className="h-4 w-full bg-[#000] rounded-[50%] opacity-20 -mt-1"></div>
+      </AdvancedMarker>
     </>
   )
 }
